0e0e863fa29b1ed76cae27bff08fcb314fae6784
Daniel <danielgtravieso@gmail.com>
2018-04-11 21:27:28 -0300

    change push button to switch

--- lab7/shift_register_board.vhd
+++ lab7/shift_register_board.vhd
@@ -4,8 +4,7 @@ use work.shift_register;
 
 entity shift_register_board is 
 	port (
-		SW : in std_logic_vector(8 downto 0);
-		KEY : in std_logic_vector(0 downto 0);
+		SW : in std_logic_vector(9 downto 0);
 		LEDR : out std_logic_vector(5 downto 0)
 	);
 end shift_register_board;
@@ -13,7 +12,7 @@ end shift_register_board;
 architecture structure of shift_register_board is 
 begin
 	shift: shift_register generic map (N=>6) port map (
-		clk => KEY(0),
+		clk => SW(9),
 		mode => SW(8 downto 7),
 		ser_in => SW(6),
 		par_in => SW(5 downto 0),
