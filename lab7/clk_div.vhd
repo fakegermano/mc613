bb92251446c51352659098876e3b7b34e1fc94d5
Daniel <danielgtravieso@gmail.com>
2018-04-11 23:21:13 -0300

    tested mouse and worked on 1Hz clock

--- lab7/clk_div.vhd
+++ lab7/clk_div.vhd
@@ -3,12 +3,33 @@ use ieee.std_logic_1164.all;
 
 entity clk_div is
   port (
-    clk : in std_logic;
-    clk_hz : out std_logic
+    clk : in std_logic; -- 50 MHz
+    clk_hz : out std_logic -- 1 Hz
   );
 end clk_div;
 
 architecture behavioral of clk_div is
 begin
-  -- Your code here!
+	process (clk)
+		constant divider: integer := 50000000; -- each 50M clock cycles toggle clk_hz (pulse each 1 second)
+		variable count: integer range 0 to divider := 0;
+		variable hz: std_logic := '0';
+		variable wid: std_logic := '0'; -- controll the width of the 1 Hz pulse
+	begin
+		if rising_edge(clk) then
+			if count = divider then
+				hz := not(hz);
+				count := 0;
+			end if;
+			count := count + 1;
+		end if;
+		if falling_edge(clk) then
+			if hz = '1' then -- if the 1 Hz pulse is up and the clock is falling, cut the pulse
+				wid := '1';
+			else
+				wid := '0';
+			end if;
+		end if;
+		clk_hz <= hz XOR wid; -- limit the pulso of hz to be the width of the clk
+	end process;
 end behavioral;
